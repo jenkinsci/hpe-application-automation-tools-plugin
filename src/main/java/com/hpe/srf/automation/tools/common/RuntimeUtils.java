b57d58abf810bf1e742e0855a0035d81a58ecdfc
Vladimir <vlad.shep@gmail.com>
2017-09-14 12:54:16 +0300

    Add SRF support

---
+++ src/main/java/com/hpe/srf/automation/tools/common/RuntimeUtils.java
@@ -0,0 +1,10 @@
+package com.hpe.srf.automation.tools.common;
+
+public class RuntimeUtils {
+    
+    @SuppressWarnings("unchecked")
+    public static <T> T cast(Object obj) {
+        
+        return (T) obj;
+    }
+}
