61f9ea4b697bee95cc8880c5ea57e37e7e06a9ae
Vladimir <vlad.shep@gmail.com>
2017-09-17 10:50:46 +0300

    Revert wrong change of ResultsPublisherModel

--- src/main/java/com/hpe/application/automation/tools/model/ResultsPublisherModel.java
+++ src/main/java/com/hpe/application/automation/tools/model/ResultsPublisherModel.java
@@ -5,22 +5,24 @@
 
 package com.hpe.application.automation.tools.model;
 
-import org.kohsuke.stapler.DataBoundConstructor;
-
 import java.io.Serializable;
 import java.util.Arrays;
 import java.util.List;
+
+import org.kohsuke.stapler.DataBoundConstructor;
 
 public class ResultsPublisherModel implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
-	public final static EnumDescription dontArchiveResults = new EnumDescription("DONT_ARCHIVE_TEST_REPORT", "Do not archive HP test reports");
-	public final static EnumDescription alwaysArchiveResults = new EnumDescription("ALWAYS_ARCHIVE_TEST_REPORT", "Always archive HP test reports");
-	public final static EnumDescription ArchiveFailedTestsResults = new EnumDescription("ONLY_ARCHIVE_FAILED_TESTS_REPORT", "Archive HP test report for failed tests ");
-	public final static EnumDescription CreateHtmlReportResults = new EnumDescription("PUBLISH_HTML_REPORT", "Always archive and publish HP test reports (LR only)");
-	public final static List<EnumDescription> archiveModes = Arrays.asList(ArchiveFailedTestsResults,alwaysArchiveResults,CreateHtmlReportResults,dontArchiveResults);
-	
+	public final static EnumDescription dontArchiveResults = new EnumDescription("DONT_ARCHIVE_TEST_REPORT", "Do not archive HPE test reports");
+	public final static EnumDescription alwaysArchiveResults = new EnumDescription("ALWAYS_ARCHIVE_TEST_REPORT", "Always archive HPE test reports");
+	public final static EnumDescription ArchiveFailedTestsResults = new EnumDescription("ONLY_ARCHIVE_FAILED_TESTS_REPORT", "Archive HPE test report for failed tests ");
+	public final static EnumDescription CreateHtmlReportResults = new EnumDescription("PUBLISH_HTML_REPORT", "Always archive and publish HPE test reports (LR only)");
+    public final static List<EnumDescription> archiveModes =
+            Arrays.asList(ArchiveFailedTestsResults, alwaysArchiveResults,
+                    CreateHtmlReportResults, dontArchiveResults);
+
 	private String archiveTestResultsMode;
 
 	@DataBoundConstructor
