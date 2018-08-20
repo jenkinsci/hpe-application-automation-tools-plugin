d9df6a620cd787149a15df5c7583b86c170c2576
Benavente <jbenavem@extern.isban.com>
2018-08-20 13:17:14 +0200

    JENKINS-53116 Include user-fields To Uploads archived junit results from Jenkinsfile

--- src/main/java/com/microfocus/application/automation/tools/results/service/IExternalEntityUploadService.java
+++ src/main/java/com/microfocus/application/automation/tools/results/service/IExternalEntityUploadService.java
@@ -22,8 +22,17 @@
 
 package com.microfocus.application.automation.tools.results.service;
 
+import java.util.Map;
+
 public interface IExternalEntityUploadService {
 
-	public void UploadExternalTestSet(AlmRestInfo loginInfo, String reportFilePath, String testsetFolderPath, String testFolderPath, String testingFramework, String testingTool, String subversion, String jobName, String buildUrl) throws ExternalEntityUploadException;	
+	public void UploadExternalTestSet(AlmRestInfo loginInfo, String reportFilePath, String testsetFolderPath,
+			String testFolderPath, String testingFramework, String testingTool, String subversion, String jobName,
+			String buildUrl) throws ExternalEntityUploadException;
+
+	public void UploadExternalTestSet(AlmRestInfo loginInfo, String reportFilePath, String testsetFolderPath,
+			String testFolderPath, String testingFramework, String testingTool, String subversion, String jobName, String buildUrl, Map<String, String> testSetFields,
+			Map<String, String> testCaseFields)
+			throws ExternalEntityUploadException;
 
 }
--- src/main/java/com/microfocus/application/automation/tools/results/service/almentities/AlmTestSetImpl.java
+++ src/main/java/com/microfocus/application/automation/tools/results/service/almentities/AlmTestSetImpl.java
@@ -23,8 +23,17 @@
 package com.microfocus.application.automation.tools.results.service.almentities;
 
 public class AlmTestSetImpl extends AlmEntityImpl implements AlmTestSet {
-	private static String restPrefix = "test-sets"; 
+	private static String restPrefix = "test-sets";
+
 	public String getRestPrefix() {
 		return restPrefix;
 	}
+
+	@Override
+	public String toString() {
+		return "AlmTestSetImpl [getName()=" + getName() + ", getId()=" + getId() + ", getRelatedEntities()="
+				+ getRelatedEntities() + ", toString()=" + super.toString() + ", getClass()=" + getClass()
+				+ ", hashCode()=" + hashCode() + "]";
+	}
+
 }
