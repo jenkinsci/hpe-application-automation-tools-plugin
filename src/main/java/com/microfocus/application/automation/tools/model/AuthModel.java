824ac3ac897f7e7ac80fbc0b881b829c0601dbaf
Andrei-Viorel Draghici <adraghici@opentext.com>
2025-02-19 11:06:53 +0200

    Delete comments

--- src/main/java/com/microfocus/application/automation/tools/model/AuthModel.java
+++ src/main/java/com/microfocus/application/automation/tools/model/AuthModel.java
@@ -46,16 +46,14 @@ import java.io.Serializable;
 public class AuthModel implements Serializable {
     private final String mcUserName;
     private final Secret mcPassword;
-    //private final String mcTenantId;
     private final Secret mcExecToken;
     private final String value;
     private AuthType authType;
 
     @DataBoundConstructor
-    public AuthModel(String mcUserName, String mcPassword, /*String mcTenantId,*/ String mcExecToken, String value) {
+    public AuthModel(String mcUserName, String mcPassword, String mcExecToken, String value) {
         this.mcUserName = mcUserName;
         this.mcPassword = Secret.fromString(mcPassword);
-        //this.mcTenantId = mcTenantId;
         this.mcExecToken = Secret.fromString(mcExecToken);
         this.value = value;
         authType = AuthType.fromString(value);
@@ -79,10 +77,6 @@ public class AuthModel implements Serializable {
             return null;
         }
     }
-
-//    public String getMcTenantId() {
-//        return mcTenantId;
-//    }
 
     public String getMcExecToken() {
         if (null != mcExecToken) {
