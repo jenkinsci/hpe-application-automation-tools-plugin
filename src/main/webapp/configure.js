8f89d0ce9815632086ebf2944b7c40ac0fe65cf8
Andrei-Viorel Draghici <adraghici@opentext.com>
2025-04-08 19:34:07 +0300

    [Jenkins Plugin] After click "Hide UFT-Specific Settings", "UFT-Specific Settings" can not show up again

--- src/main/webapp/configure.js
+++ src/main/webapp/configure.js
@@ -259,8 +259,19 @@ async function loadMobileInfo(a, b, o, err) {
 function hideAndMoveAdvancedBody(_id) {
     const tBody = document.querySelector("#" + _id).parentNode; // initial advanced block content
     const initialAdvancedBlock = tBody.previousSibling; // advanced link button block and here was hidden the initial advanced block content
+    const advancedLink = initialAdvancedBlock.querySelector(".advancedLink");
     initialAdvancedBlock.querySelector(".advancedBody").appendChild(tBody); // moves the initial advanced block content back to the hidden block
-    initialAdvancedBlock.querySelector(".advancedLink").style.display = ""; // enables once again the advanced link
+    initialAdvancedBlock.querySelector(".advancedLink").style = ""; // enables once again the advanced link
+
+    const advancedButton = advancedLink.querySelector(".advancedLink .jenkins-button.advanced-button.advancedButton");
+    if (advancedButton) {
+        advancedButton.removeAttribute("data-expanded");
+    }
+
+    const dropdownContainer = initialAdvancedBlock.querySelector(".advancedBody .tbody.dropdownList-container");
+    if (dropdownContainer) {
+        dropdownContainer.removeAttribute("style");
+    }
 }
 
 async function loadRegionDDL(dlg, regions) {
